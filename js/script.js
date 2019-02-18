00759221a872720a55dfae86615d3fbddf48489e
Aleksandra Dworak <aaleksandra.dworak@gmail.com>
2019-02-18 16:10:29 +0100

    16.6 v.2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 var url = 'https://restcountries.eu/rest/v2/name/';
 var countriesList = document.getElementById('countries');
+var SVG_NS = 'http://www.w3.org/2000/svg';
+var XLink_NS = 'http://www.w3.org/1999/xlink';
 document.getElementById('search').addEventListener('click', searchCountries);
 
 function searchCountries() {
@@ -16,7 +18,18 @@ function showCountriesList(resp) {
     countriesList.innerHTML = '';
     resp.forEach(function(item) {
         var liEl = document.createElement('li');
-        liEl.innerText = item.name + ', capital city:' + item.capital;
+        liEl.innerHTML = item.name + ', capital city:' + item.capital;
+
+        var svg = document.createElementNS(SVG_NS, "svg");
+        svg.setAttribute('width', 20);
+        svg.setAttribute('height', 15);
+        var flag = document.createElementNS(SVG_NS, 'image');
+        flag.setAttributeNS(null, 'width', '20');
+        flag.setAttributeNS(null, 'height', '15');
+        flag.setAttributeNS(XLink_NS, 'xlink:href', item.flag);
+        svg.appendChild(flag);
+       
+        liEl.appendChild(svg);
         countriesList.appendChild(liEl);
     });
 }
